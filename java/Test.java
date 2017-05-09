a61f46ea957f7b8d8bcf74c30206bb0d81e3ed87
jw <jwnba24@163.com>
2017-05-09 10:59:15 +0800

    0.2

---
+++ java/Test.java
@@ -0,0 +1,13 @@
+package java;
+
+import java.util.Scanner;
+
+/**
+ * Created by Admini
+ * strator on 2017/5/9.
+ */
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+    }
+}
